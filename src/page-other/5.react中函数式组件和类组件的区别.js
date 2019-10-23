1f6537545991231220a3c74fcbfad91e62c82487
tlzj <quesanbo@netposa.com>
2019-10-23 13:32:10 +0800

    react中函数组件和类组件的区别，以及hook相关

---
+++ src/page-other/5.react中函数式组件和类组件的区别.js
@@ -0,0 +1,28 @@
+function Other(props){
+  function clickHandel(){
+    setTimeout(() => {
+      console.log(props); // 打印的是上一次的props数据
+    }, 5000)
+  }
+  return (
+    <div onClick={clickHandel}>其他测试</div>
+  )
+}
+
+class Other1 extends React.Component {
+  clickHandel = () => {
+    setTimeout(() => {
+      console.log(this.props); // 打印的是实时的props数据
+    }, 5000)
+  }
+  render(){
+    return (
+      <div onClick={this.clickHandel.bind(this)}>其他测试-1</div>
+    )
+  }
+}
+
+// 相关阅读
+// https://github.com/xiaohesong/TIL/blob/master/front-end/react/overreact/%E5%87%BD%E6%95%B0%E7%BB%84%E4%BB%B6%E4%B8%8E%E7%B1%BB%E6%9C%89%E4%BB%80%E4%B9%88%E4%B8%8D%E5%90%8C.md
+// hook中的一点小点
+// https://blog.csdn.net/hesongGG/article/details/88824592
